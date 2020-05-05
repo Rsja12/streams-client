35e88c4c3b08d2725b597416befb8cf8ff97b064
Rsja12 <asocarras10@gmail.com>
2020-05-05 14:17:20 -0400

    import stream components into app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { BrowserRouter, Route, Link } from 'react-router-dom'
 
-
+import StreamCreate from './streams/StreamCreate'
+import StreamDelete from './streams/StreamDelete'
+import StreamEdit from './streams/StreamEdit'
+import StreamList from './streams/StreamList'
+import StreamShow from './streams/StreamShow'
 
 const App = () => {
     return (
         <div>
             <BrowserRouter>
                 <div>
-                    <Route path="/" exact component={PageOne} />
-                    <Route path="/pageTwo" component={PageTwo} />
+                    
+                    
                 </div>
             </BrowserRouter>
         </div>
