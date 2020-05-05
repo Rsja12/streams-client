9b912cca7150fbc11cac216909582c0e68af42b6
Rsja12 <asocarras10@gmail.com>
2020-05-05 14:53:55 -0400

    import Header in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,12 @@ import StreamDelete from './streams/StreamDelete'
 import StreamEdit from './streams/StreamEdit'
 import StreamList from './streams/StreamList'
 import StreamShow from './streams/StreamShow'
+import Header from './Header'
 
 const App = () => {
     return (
         <div>
+            <Header />
             <BrowserRouter>
                 <div>
                     <Route path='/' exact component={StreamList} />
