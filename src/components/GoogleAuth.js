366f75432354ff55dca341f2910878492177cf8b
Rsja12 <asocarras10@gmail.com>
2020-05-06 13:59:46 -0400

    add login/logout buttons

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -31,18 +31,28 @@ export class GoogleAuth extends Component {
 
     renderAuthBtn() {
         if ( this.state.isSignedIn === null ) {
-            return <div>I don't know if we are signed in</div>
+            return null
         } else if ( this.state.isSignedIn ) {
-            return <div>I am signed in!</div>
+            return(
+                <button className='ui red google button'>
+                    <i className='google icon' />
+                    Sign Out
+                </button>
+            )
         } else {
-            return <div>I am not signed in</div>
+            return(
+                <button className='ui red google button'>
+                    <i className='google icon' />
+                    Sign In
+                </button>
+            )
         }
     }
 
     render() {
         return (
             <div>
-                {this.renderAuthBtn()}
+                { this.renderAuthBtn() }
             </div>
         )
     }
