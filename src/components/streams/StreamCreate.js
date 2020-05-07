4076a8eb77c4a0a956996345456f21573be7ac91
Rsja12 <asocarras10@gmail.com>
2020-05-07 18:26:39 -0400

    refactor destructuring of props of <input /> in renderInput

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
 
 export class StreamCreate extends Component {
+
+    // Destructure input key (which is an obj of objs) from formProps obj
+    renderInput({ input }) {
+        console.log(input)
+        // return (
+        //     <input 
+        //     onChange={formProps.input.onChange} 
+        //     value={formProps.input.value} />
+        // )
+        // grab every prop and add it to input
+        return <input {...input} />
+    }
+
     render() {
         return (
             <form>
-                <Field name='Title' />
-                <Field name='Description' />
+                <Field name='Title' component={this.renderInput} />
+                <Field name='Description' component={this.renderInput} />
             </form>
         )
     }
