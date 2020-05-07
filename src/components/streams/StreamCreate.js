8596df60c3be5a967cb919f1cc85870b2b1e00e2
Rsja12 <asocarras10@gmail.com>
2020-05-07 18:38:41 -0400

    add labels to fields and add styling

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -4,7 +4,8 @@ import { Field, reduxForm } from 'redux-form'
 export class StreamCreate extends Component {
 
     // Destructure input key (which is an obj of objs) from formProps obj
-    renderInput({ input }) {
+    // label comes from the label prop in the Field elements because Field doesn't know what to do with it, it passes that prop to the function being called
+    renderInput({ input, label }) {
         console.log(input)
         // return (
         //     <input 
@@ -12,14 +13,19 @@ export class StreamCreate extends Component {
         //     value={formProps.input.value} />
         // )
         // grab every prop and add it to input
-        return <input {...input} />
+        return(
+            <div className='field'>
+                <label>{ label }</label>
+                <input {...input} />
+            </div>
+        )
     }
 
     render() {
         return (
-            <form>
-                <Field name='Title' component={this.renderInput} />
-                <Field name='Description' component={this.renderInput} />
+            <form className='ui form'>
+                <Field name='Title' label='Enter Title' component={this.renderInput} />
+                <Field name='Description' label='Enter Description' component={this.renderInput} />
             </form>
         )
     }
