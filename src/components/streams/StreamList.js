41873c5c923519035f7269ebbab0970c9706627e
Rsja12 <asocarras10@gmail.com>
2020-05-11 11:49:35 -0400

    add edit and delete buttons

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -8,12 +8,28 @@ class StreamList extends React.Component {
         this.props.fetchStreams()
     }
 
+    renderAdminBtns = stream => {
+        if( stream.userId === this.props.currentUserId ) {
+            return(
+                <div className='right floated content'>
+                    <button className='ui button primary'>
+                        Edit
+                    </button>
+                    <button className='ui button negative'>
+                        Delete
+                    </button>
+                </div>
+            )
+        }
+    }
+
     renderList() {
         return this.props.streams.map( stream => {
             return(
                 <div className='item' key={stream.id}>
+                    { this.renderAdminBtns(stream) }
                     <i className='large middle aligned icon camera' />
-                    <div className='conent'>
+                    <div className='content'>
                         {stream.title}
                         <div className='description'>
                             {stream.description}
