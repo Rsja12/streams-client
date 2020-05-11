e1dc0a4d3bc0c7c02bf5228192a6898de856edc9
Rsja12 <asocarras10@gmail.com>
2020-05-11 10:46:20 -0400

    add streamReducer to combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 
 import authReducer from './authReducer'
+import streamReducer from './streamReducer'
 
 export default combineReducers({
     auth: authReducer,
     // formReducer is created by redux-form and must have the key of 'form'
-    form: formReducer
+    form: formReducer,
+    streams: streamReducer
 })
 
