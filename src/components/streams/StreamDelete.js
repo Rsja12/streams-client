7818686cea91dbd59ed0863a706102f2d2a56679
Rsja12 <asocarras10@gmail.com>
2020-05-12 14:00:12 -0400

    implements button functionality on modal

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 import { fetchStream, deleteStream } from '../../actions'
 
 import Modal from '../Modal'
@@ -8,32 +9,47 @@ import history from '../../history'
 class StreamDelete extends React.Component {
 
     componentDidMount() {
-        this.props.fetchStream(this.props.match.params.id)
+        this.props.fetchStream( this.props.match.params.id )
     }
     
     renderActions() {
+        const { id } = this.props.match.params
         return (
             // React.Fragment allows us to wrap multiple tags while creating an 'invisible' dom element. It doesn't show up on the dom so it doesn't mess with semantic ui's styling
             <React.Fragment>
-                <button className='ui button negative'>Delete</button>
-                <button className='ui button'>Cancel</button>
+
+                <button 
+                onClick={ () => this.props.deleteStream( id ) }
+                className='ui button negative'>
+                    Delete
+                </button>
+
+                <Link 
+                to='/'
+                className='ui button'>
+                    Cancel
+                </Link>
+
             </React.Fragment>
         )
     }
+
+    renderContent() {
+        if( !this.props.stream ) {
+            return 
+        }
+
+        return `Are you sure you want to delete the stream with the title: ${this.props.stream.title}`
+    }
     
     render(){
-        console.log(this.props.stream)
         return (
-            <div>
-    
                 <Modal 
                     title='Delete Stream'
-                    content='Are you sure you want to delete this stream?'
+                    content={ this.renderContent() }
                     actions={ this.renderActions() }
                     onDismiss={ () => history.push('/') }
                 />
-    
-            </div>
         )
     }
 }
