ce89df54ff28b66cfa15b74693fe207db7382ef9
Rsja12 <asocarras10@gmail.com>
2020-05-12 15:53:09 -0400

    wrap Routes in Switch

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchStream } from '../../actions'
 
 export class StreamShow extends Component {
+
+    componentDidMount() {
+        this.props.fetchStream( this.props.match.params.id )
+    }
+    
     render() {
+        console.log(this.props.stream)
         return (
             <div>
                 StreamShow
@@ -10,4 +18,8 @@ export class StreamShow extends Component {
     }
 }
 
-export default StreamShow
+const mapStateToProps = ( state, ownProps ) => ({
+    stream: state.streams[ownProps.match.params.id]
+})
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow)
