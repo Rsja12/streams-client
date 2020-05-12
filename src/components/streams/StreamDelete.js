1b779cf478b37be68e47775166f3925027a59dd5
Rsja12 <asocarras10@gmail.com>
2020-05-12 10:47:44 -0400

    make Modal reusable

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -2,10 +2,25 @@ import React from 'react'
 import Modal from '../Modal'
 
 const StreamDelete = () => {
+
+    const actions = (
+        // React.Fragment allows us to wrap multiple tags while creating an 'invisible' dom element. It doesn't show up on the dom so it doesn't mess with semantic ui's styling
+        <React.Fragment>
+            <button className='ui button negative'>Delete</button>
+            <button className='ui button'>Cancel</button>
+        </React.Fragment>
+    )
+
     return (
         <div>
             StreamDelete
-            <Modal />
+
+            <Modal 
+                title='Delete Stream'
+                content='Are you sure you want to delete this stream?'
+                actions={ actions }
+            />
+
         </div>
     )
 }
