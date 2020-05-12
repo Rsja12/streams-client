9b6888d6c573241296aa55f1884ac34fc481bc95
Rsja12 <asocarras10@gmail.com>
2020-05-12 11:49:58 -0400

    make delete button a Link to take user to /streams/delete/:id

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -19,9 +19,11 @@ class StreamList extends React.Component {
                     className='ui button primary'>
                         Edit
                     </Link>
-                    <button className='ui button negative'>
+                    <Link 
+                    to={`/streams/delete/${stream.id}`}
+                    className='ui button negative'>
                         Delete
-                    </button>
+                    </Link>
                 </div>
             )
         }
